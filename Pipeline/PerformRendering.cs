5d9f262531b1fc3d1e4f18dc35fb89bd42ed1bff
Derek Temple <xalor90@outlook.com>
2018-07-13 13:56:20 -0700

    Created project

---
+++ Pipeline/PerformRendering.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Mvc.Pipelines.Response.RenderPlaceholder;
+using Sitecore.Mvc.Presentation;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Sitecore.Foundation.Extension.DynamicPlaceholderRendering.Pipeline
+{
+    public class PerformRendering : Mvc.Pipelines.Response.RenderPlaceholder.PerformRendering
+    {
+        protected override void Render(string placeholderName, TextWriter writer, RenderPlaceholderArgs args)
+        {
+            var renderingsForPlaceholder = GetRenderings(placeholderName, args).ToList();
+            if (!renderingsForPlaceholder.Any())
+            {
+                var unusedPlaceholders = Context.Items["unusedPlaceholderKeys"] as List<string> ?? new List<string>();
+                unusedPlaceholders.Add(placeholderName);
+                Context.Items["unusedPlaceholderKeys"] = unusedPlaceholders;
+            }
+
+            foreach (Rendering rendering in renderingsForPlaceholder)
+            {
+                if (rendering != null)
+                {
+                    using (CreateCyclePreventer(placeholderName, rendering))
+                    {
+                        ProcessRenderRendering(rendering, writer);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore.Foundation.Extension.DynamicPlaceholderRendering")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("XalorTech")]
+[assembly: AssemblyProduct("Sitecore.Foundation.Extension.DynamicPlaceholderRendering")]
+[assembly: AssemblyCopyright("Copyright © XalorTech 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("771386f1-559d-4a9e-88c8-8ce6983afeb1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
